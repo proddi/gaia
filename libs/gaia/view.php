13f417686e60b7056e5b412ce739acb6fe55340e
Proddi <proddi@splatterladder.com>
2012-04-10 16:21:02 +0200

    dump-filter now shows only public keys. inspect-filters shows all.

--- libs/gaia/view.php
+++ libs/gaia/view.php
@@ -43,13 +43,45 @@ class gaiaView {
     }
 
     public static function filters() {
+        $dump;
         return new gaiaInvokable(array(
             'capitalize' => function($str) { return ucwords(strtolower($str)); },
             'truncate' => function($limit, $str) { if (strlen($str) <= $limit) return $str; return substr($str, 0, $limit - 3) . '...'; },
             'join' => function($arr, $glue = ', ') { return implode($glue, $arr); },
             'explode' => function($arr, $glue = ', ') { return explode($glue, $arr); },
             'first' => function($arr) { return $arr[0]; },
-            'dump' => function($data) { return highlight_string('<? '. var_export($data, true) . ' ?>', true); },
+            'inspect' => function($data) { return highlight_string('<? '. var_export($data, true) . ' ?>', true); },
+            'dump' => $dump = function($var, $root = true, $intend = 0) use (&$dump) {
+                $c = function ($text, $color, $root = false) { return '<span style="color: ' . $color . '">' . $text . '</span>'; };
+                $pre = $root ? '<code>' : '';
+                $post = $root ? '</code>' : '';
+                $preBlock = $root ? '<pre><code>' : '';
+                $postBlock = $root ? '</code></pre>' : '';
+                $id = '  ';
+                $is = str_repeat($id, $intend);
+                if (is_null($var)) return $pre . $c('NULL', 'blue') . $post;
+                if (is_numeric($var)) return $pre . $c($var, 'blue') . $post;
+                if (is_string($var)) return $pre . $c("'" . addslashes($var) . "'", 'red') . $post;
+                if (is_bool($var)) return $pre . $c($var ? 'true' : 'false' , 'blue') .$post;
+                if (is_resource($var)) return $pre . $c('resource of type (' . get_resource_type($var). ')', 'blue') . $post;
+                if (is_array($var)) {
+                    $markup = $c('array (', 'green') . "\n";
+                    foreach (array_keys($var) as $key) {
+                        $markup .= $is . $id . $dump($key, false) . ' => ' . $dump($var[$key], false, $intend+1) . ",\n";
+                    }
+                    $markup .= $is . $c(')', 'green');
+                    return $preBlock . $markup . $postBlock;
+                }
+                if (is_object($var)) {
+                    $markup = $c('object('. get_class($var). ')#' . substr(spl_object_hash($var), 15, 8) .' {', 'blue') . "\n";
+                    foreach (get_object_vars($var) as $key => $val) {
+                        $markup .= $is . $id . $c($key, 'green') . ': ' . $dump($var->$key, false, $intend+1) . "\n";
+                    }
+                    $markup .= $is . $c('}', 'blue');
+                    return $preBlock . $markup . $postBlock;
+                }
+                return $pre . $var . $post;
+             },
             'upper' => function($str) { return strtoupper($str); },
             'lower' => function($str) { return strtolower($str); },
             'escape' => function($str) { return htmlentities($str); },
